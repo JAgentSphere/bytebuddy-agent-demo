477dfc7a75de1eacca24b520ebf7fd15eb875093
ReaJason <reajason1225@gmail.com>
2024-02-04 00:04:36 +0800

    feat: support cmd detection

--- src/main/java/com/jas/agent/Main.java
+++ src/main/java/com/jas/agent/Main.java
@@ -5,6 +5,9 @@ import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatchers;
 
 import java.lang.instrument.Instrumentation;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author ReaJason
@@ -37,6 +40,40 @@ public class Main {
                                 Advice.to(SpringStartupInterceptor.class)
                                         .on(ElementMatchers.named("getStartingMessage"))))
                 ).installOn(inst);
+
+
+        agentBuilder.type(ElementMatchers.named("java.lang.Runtime"))
+                .transform(((builder, typeDescription, classLoader, module, protectionDomain) ->
+                        builder.visit(
+                                Advice.to(RuntimeExecInterceptor.class)
+                                        .on(ElementMatchers.named("exec")
+                                                .and(ElementMatchers.takesArguments(1)))
+                        ))).installOn(inst);
+    }
+
+    private static class RuntimeExecInterceptor {
+        @Advice.OnMethodEnter
+        public static void interceptor(@Advice.AllArguments Object[] args) {
+            System.out.println("Runtime.exec is invoked");
+            String command = (String) args[0];
+            System.out.println("Runtime.exec arg is " + command);
+
+            // 方法参数的判断, 执行 whoami 会在这儿抛出异常
+            if ("whoami".equals(command)) {
+                throw new SecurityException("the command whoami is prohibited in this env");
+            }
+
+            // 获取堆栈执行上下文进行特定的判断
+            List<String> stackTraces = Arrays.stream(new Throwable().getStackTrace()).limit(100)
+                    .map(StackTraceElement::toString).collect(Collectors.toList());
+
+            // 所有的命令都会被阻断，因为堆栈中包含了这个
+            for (String stackTrace : stackTraces) {
+                if (stackTrace.contains("com.jas.web.demo.IndexController.cmd")) {
+                    throw new SecurityException("exec command with dangerous stack");
+                }
+            }
+        }
     }
 
     private static class SpringStartupInterceptor {
